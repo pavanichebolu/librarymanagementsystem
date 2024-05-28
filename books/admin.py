d395885856db95bd6dddd5270d1a61e389f08039
pavanichebolu <pavanichebolu6214@gmail.com>
2024-05-28 15:21:49 +0530

    first commit

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Book
-
+from authors.models import Author
+from loans.models import Loan
 
 admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Loan)
 
 # Register your models here.
