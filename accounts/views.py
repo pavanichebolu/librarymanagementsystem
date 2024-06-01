1a541ca5d2d269b55ed1de2bde164eb242485639
pavanichebolu <pavanichebolu6214@gmail.com>
2024-06-01 10:01:58 +0530

    login form created

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render,redirect
-
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages,auth
 # Create your views here.
 def register(request):
     return render(request, 'register.html')
+
+
+
+
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('books')  # Adjust this to redirect to the appropriate dashboard based on user type
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
 def logout(request):
-  return redirect('/')
+    auth.logout(request)
+    return redirect('/')
--- authors/models.py
+++ authors/models.py
@@ -8,7 +8,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     
-    def _str_(self):
+    def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 # Create your models here.
